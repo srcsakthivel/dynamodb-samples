a2d9c0682ec6bf413a78556bc0398d2a93deed09
workshop-1c71e191-b6a2-4a53-8cf0-4fea15eb651b <user@codecommit.aws>
2024-01-14 18:23:50 -0800

    Store WebApi

---
+++ Store.Api/Controllers/StoreController.cs
@@ -0,0 +1,33 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Store.Api.Model;
+
+namespace Store.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StoreController : ControllerBase
+    {
+        private readonly ILogger<StoreController> _logger;
+        private IDynamoDBContext _dynamoDbContext;
+
+        public StoreController(IDynamoDBContext dynamoDBContext, ILogger<StoreController> logger)
+        {
+            _dynamoDbContext = dynamoDBContext;
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetStoreItems")]
+        public IEnumerable<StoreModel> Get()
+        {
+            return _dynamoDbContext.ScanAsync<StoreModel>(default).GetRemainingAsync().Result;
+        }
+
+        [HttpGet("{Id}")]
+        public StoreModel GetItem(string Id)
+        {
+            return _dynamoDbContext.LoadAsync<StoreModel>(Id).Result;
+        }
+    }
+}
---
+++ Store.Api/Model/StoreModel.cs
@@ -0,0 +1,15 @@
+﻿using Amazon.DynamoDBv2.DataModel;
+
+namespace Store.Api.Model
+{
+    [DynamoDBTable("Store")]
+    public class StoreModel
+    {
+        public string Id { get; set; }
+
+        [DynamoDBProperty("Name")]
+        public string AlbumName { get; set; }
+        public string Artist { get; set; }
+        public string ReleaseDate { get; set; }
+    }
+}
