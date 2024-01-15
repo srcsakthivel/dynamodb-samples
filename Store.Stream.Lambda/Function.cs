8398516fa56330628e30a6dc6cfb6e3c523b88b5
workshop-1c71e191-b6a2-4a53-8cf0-4fea15eb651b <user@codecommit.aws>
2024-01-15 00:39:17 -0800

    DB Stream Sample

---
+++ Store.Stream.Lambda/Function.cs
@@ -0,0 +1,29 @@
+using Amazon.Lambda.Core;
+using Amazon.Lambda.DynamoDBEvents;
+using Amazon.DynamoDBv2.Model;
+using AWS.Lambda.Powertools.Logging;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace Store.Stream.Lambda;
+
+public class Function
+{
+    [Logging(LogEvent = true)]
+
+    public void FunctionHandler(DynamoDBEvent dynamoEvent, ILambdaContext context)
+    {
+        context.Logger.LogInformation($"Beginning to process {dynamoEvent.Records.Count} records...");
+
+        foreach (var record in dynamoEvent.Records)
+        {
+            context.Logger.LogInformation($"Event ID: {record.EventID}");
+            context.Logger.LogInformation($"Event Name: {record.EventName}");
+            
+            // TODO: Add business logic processing the record.Dynamodb object.
+        }
+
+        context.Logger.LogInformation("Stream processing complete.");
+    }
+}
